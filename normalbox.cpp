383a80f62d0bd1ca785f9d1cb4d19dfb95b9b5d3
amirreza <taghaddosiamirreza@gmail.com>
2021-02-25 20:54:35 +0330

    add body for functions of NormalBox class

--- normalbox.cpp
+++ normalbox.cpp
@@ -4,3 +4,18 @@ NormalBox::NormalBox()
 {
 
 }
+
+void NormalBox::set_pos(sf::Vector2f position)
+{
+    this->position = position;
+}
+
+sf::Vector2f NormalBox::get_pos() const
+{
+    return position;
+}
+
+int NormalBox::get_move() const
+{
+    return move;
+}
