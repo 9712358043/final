ddc142e2530290b0a8434a870889fdf4b25927ee
amirreza <taghaddosiamirreza@gmail.com>
2021-02-25 20:58:50 +0330

    add body for functions of XBox class

--- xbox.cpp
+++ xbox.cpp
@@ -4,3 +4,18 @@ XBox::XBox()
 {
 
 }
+
+void XBox::set_pos(sf::Vector2f position)
+{
+    this->position = position;
+}
+
+sf::Vector2f XBox::get_pos() const
+{
+    return position;
+}
+
+int XBox::get_move() const
+{
+    return move;
+}
